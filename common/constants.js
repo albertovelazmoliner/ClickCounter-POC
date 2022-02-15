dbc6c10f3d447119a32b3177703de96ecab7cd9c
Alberto Vélaz <albertovelaz.dev@gmail.com>
2022-02-15 07:35:03 +0100

    Update constants.js

--- common/constants.js
+++ common/constants.js
@@ -9,8 +9,10 @@ const TIMESTAMP = {
 };
 const KEY_IP = 0;
 const LIMIT_IP_NUMBER = 10;
+const CLICK_DATE_REGEX = '^([1-9]|([012][0-9])|(3[01])).([0]{0,1}[1-9]|1[012]).\\d\\d\\d\\d [012]{0,1}[0-9]:[0-6][0-9]:[0-6][0-9]$';
 
 module.exports = {
+  CLICK_DATE_REGEX,
   FILE_DATA_CLICK,
   KEY_IP,
   LIMIT_IP_NUMBER,
