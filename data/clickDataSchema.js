7de43688aad81a61370ab840a287c10ac9912efb
Alberto Vélaz <albertovelaz.dev@gmail.com>
2022-02-15 07:35:07 +0100

    Update clickDataSchema.js

--- data/clickDataSchema.js
+++ data/clickDataSchema.js
@@ -1,10 +1,12 @@
+const { CLICK_DATE_REGEX } = require('../common/constants');
+
 const schema = {
   type: 'object',
   properties: {
     ip: { type: 'string' },
     timestamp: {
       type: 'string',
-      pattern: '^([1-9]|([012][0-9])|(3[01])).([0]{0,1}[1-9]|1[012]).\\d\\d\\d\\d [012]{0,1}[0-9]:[0-6][0-9]:[0-6][0-9]$',
+      pattern: CLICK_DATE_REGEX,
     },
     amount: {
       type: 'number',
