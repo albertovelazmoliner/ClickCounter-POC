e7515da194c2035cd3e198f67552d2b5a1e0e735
Alberto Vélaz <albertovelaz.dev@gmail.com>
2022-02-15 22:04:59 +0100

    Update dataReader.js

--- data/dataReader.js
+++ data/dataReader.js
@@ -1,25 +1,16 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
-async function reader(pathToFile, callback) {
-  fs.readFile(pathToFile, (err, data) => {
-    if (err) {
-      throw err;
-    }
-    const bufferString = data.toString();
-    const fileData = JSON.parse(bufferString);
-    callback(fileData);
-  });
-}
-
-const fileReader = async (pathToFile, callback) => reader(pathToFile, callback);
-
-const fileReaderSync = (pathToFile) => {
-  const data = fs.readFileSync(pathToFile, 'utf8');
-  const readData = JSON.parse(data);
-  return readData;
+const fileReader = async function reader(pathToFile) {
+  try {
+    const data = await fs.readFile(pathToFile, { encoding: 'utf-8' });
+    return JSON.parse(data);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    throw error;
+  }
 };
 
 module.exports = {
   fileReader,
-  fileReaderSync,
 };
