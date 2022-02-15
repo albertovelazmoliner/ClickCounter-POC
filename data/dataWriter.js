a546e7acd5c0308c1d289c0e1c0f0c3507614ded
Alberto Vélaz <albertovelaz.dev@gmail.com>
2022-02-15 22:05:01 +0100

    Update dataWriter.js

--- data/dataWriter.js
+++ data/dataWriter.js
@@ -1,13 +1,15 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
-const writeData = (data) => {
-  fs.writeFile('result​set.json', JSON.stringify(data), (err) => {
-    if (err) {
-      throw err;
-    }
+const writeData = async (data) => {
+  try {
+    await fs.writeFile('result​set.json', JSON.stringify(data), { encoding: 'utf-8' });
     // eslint-disable-next-line no-console
     console.log('JSON data is saved.');
-  });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    throw error;
+  }
 };
 
 module.exports = {
