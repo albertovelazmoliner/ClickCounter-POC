e738201c4d3bc172c6f7a8ec5b682cfc1f2b3732
Alberto Vélaz <albertovelaz.dev@gmail.com>
2022-02-15 07:33:30 +0100

    Update clickDataSchema.js

--- data/clickDataSchema.js
+++ data/clickDataSchema.js
@@ -2,7 +2,10 @@ const schema = {
   type: 'object',
   properties: {
     ip: { type: 'string' },
-    timestamp: { type: 'string' },
+    timestamp: {
+      type: 'string',
+      pattern: '^([1-9]|([012][0-9])|(3[01])).([0]{0,1}[1-9]|1[012]).\\d\\d\\d\\d [012]{0,1}[0-9]:[0-6][0-9]:[0-6][0-9]$',
+    },
     amount: {
       type: 'number',
       multipleOf: 0.01,
